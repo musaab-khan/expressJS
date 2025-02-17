51acd73997296dc5ccc75993fca4550f7602fa28
musaab-khan <musaabkhan4511@gmail.com>
2025-02-17 22:24:01 +0500

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
