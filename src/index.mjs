d9e200032cdd49e6c1001c710dc73eec7b6fe874
musaab-khan <musaabkhan4511@gmail.com>
2025-02-17 23:51:01 +0500

    get request

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
+app.get('/',(request,response)=>{
+    response.status(200).send(
+        {'msg':'Hello!'}
+    )
+})
+
+app.get('/api/users',(request,response)=>{
+    response.status(200).send([
+        {'id':1,'name':'Musaab','fatherName':'Shahzad Ur Rehman'},
+        {'id':2,'name':'Umair','fatherName':'Hamid Gujjar'},
+        {'id':3,'name':'Saim','fatherName':'Aziz Bhatti'}
+    ])
+})
+
+app.get('/',(request,response)=>{
+    response.status(200).send({'msg':'Hello!'})
+})
+
+app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
